bb779849c1e31064062e032815e7b0e0fbe43653
Janhvi Dasila <91754052+janhvidasila@users.noreply.github.com>
2025-04-06 02:44:55 +0530

    Add files via upload

---
+++ 04_views.sql
@@ -0,0 +1,6 @@
+USE db_Churn
+Create View vw_ChurnData as
+    select * from prod_Churn where Customer_Status In ('Churned', 'Stayed')
+
+Create View vw_JoinData as
+    select * from prod_Churn where Customer_Status = 'Joined'
